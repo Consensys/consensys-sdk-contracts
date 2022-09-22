60ef02154ae4b1ab434d68db2ce2a6048b60f3a4
Fermin Carranza <fcarr004@fiu.edu>
2022-09-22 18:32:52 -0400

    feat(contracts): erc1155 udpates

--- contracts/ERC1155Mintable.sol
+++ contracts/ERC1155Mintable.sol
@@ -7,29 +7,32 @@ import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-/// Token URI of contract cannot be empty.
-error TokenURIIsEmpty();
+/// Base URI of tokens to be minted cannot be empty.
+error BaseURIIsEmpty();
+/// Contract URI cannot be empty.
+error ContractURIIsEmpty();
 
-contract ERC1155Mintable is
-    ERC1155,
-    ERC2981,
-    AccessControl,
-    Ownable
-{
+contract ERC1155Mintable is ERC1155, ERC2981, AccessControl, Ownable {
     using Strings for uint256;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     string private _baseURI;
-    mapping (uint256 => bool) private _validIds;
+    string private _contractURI;
+    mapping(uint256 => bool) private _validIds;
 
-    constructor(string memory uri_, uint256[] memory ids_) ERC1155(uri_) {
+    constructor(
+        string memory uri_,
+        string memory contractURI_,
+        uint256[] memory ids_
+    ) ERC1155(uri_) {
         if (!(bytes(uri_).length > 1)) {
-            revert TokenURIIsEmpty();
+            revert BaseURIIsEmpty();
         }
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
         _baseURI = uri_;
+        _contractURI = contractURI_;
         addIds(ids_);
     }
 
@@ -37,7 +40,7 @@ contract ERC1155Mintable is
         address to_,
         uint256 id_,
         uint256 quantity_
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    ) public onlyRole(MINTER_ROLE) {
         _mint(to_, id_, quantity_, "");
     }
 
@@ -45,7 +48,7 @@ contract ERC1155Mintable is
         address to_,
         uint256[] memory ids_,
         uint256[] memory quantities_
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    ) public onlyRole(MINTER_ROLE) {
         _mintBatch(to_, ids_, quantities_, "");
     }
 
@@ -54,13 +57,25 @@ contract ERC1155Mintable is
             _validIds[ids_[i]] = true;
         }
     }
- 
+
     ///#if_succeeds let uri := uri() in uri == newUri_;
     function setURI(string memory newUri_) public onlyRole(DEFAULT_ADMIN_ROLE) {
         if (!(bytes(newUri_).length > 1)) {
-            revert TokenURIIsEmpty();
+            revert BaseURIIsEmpty();
         }
         _baseURI = newUri_;
+    }
+
+    ///#if_succeeds (keccak256(abi.encodePacked((_contractURI))) != keccak256(abi.encodePacked((""))));
+    ///#if_succeeds (keccak256(abi.encodePacked((_contractURI))) == keccak256(abi.encodePacked((contractURI_))));
+    function setContractURI(string memory contractURI_)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        if (!(bytes(contractURI_).length > 1)) {
+            revert ContractURIIsEmpty();
+        }
+        _contractURI = contractURI_;
     }
 
     ///#if_succeeds let receiver, _ := royaltyInfo(0, 10000) in receiver == receiver_;
@@ -71,19 +86,22 @@ contract ERC1155Mintable is
         _setDefaultRoyalty(receiver_, feeNumerator_);
     }
 
+    function contractURI() public view returns (string memory) {
+        return _contractURI;
+    }
+
     function uri(uint256 tokenId_)
         public
-        view                
+        view
         override
         returns (string memory)
     {
-        require(
-            _validIds[tokenId_],
-            "URI requested for invalid Token ID"
-        );
+        require(_validIds[tokenId_], "URI requested for invalid Token ID");
         return
             bytes(_baseURI).length > 0
-                ? string(abi.encodePacked(_baseURI, tokenId_.toString(), ".json"))
+                ? string(
+                    abi.encodePacked(_baseURI, tokenId_.toString(), ".json")
+                )
                 : _baseURI;
     }
 
